6fb8d7913343ca7c16490f0e3424cfc7574109a5
Xuyan Li <lxuy1999@sjtu.edu.cn>
2026-06-06 17:46:34 +0800

    finish lab-fs

--- user/user.h
+++ user/user.h
@@ -31,6 +31,7 @@ int trace(int);
 int sysinfo(struct sysinfo *);
 int sigalarm(int ticks, void (*handler)());
 int sigreturn(void);
+int symlink(char *, char *); // created in lab-fs
 #ifdef LAB_NET
 int connect(uint32, uint16, uint16);
 #endif
